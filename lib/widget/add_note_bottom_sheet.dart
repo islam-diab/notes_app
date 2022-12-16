d43ba23714d8b715683ae761f3607381823604f6
Islam Diab <islamdiab304@gmail.com>
2022-12-16 20:24:10 +0200

    i'm sorry

--- lib/widget/add_note_bottom_sheet.dart
+++ lib/widget/add_note_bottom_sheet.dart
@@ -3,7 +3,6 @@ import 'package:notes_app/widget/custom_text_fild.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Padding(
