d2c9ead51ab9b2ec17a49382eb9d9c5820645066
Islam Diab <islamdiab304@gmail.com>
2022-12-16 21:20:52 +0200

    i'm  so so sorry remove

---
+++ lib/views/widgets/custom_text_field.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    Key? key,
+    required this.hint,
+    this.maxLines = 1,
+  }) : super(key: key);
+
+  final String hint;
+  final int maxLines;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+      maxLines: maxLines,
+      decoration: InputDecoration(
+        hintText: hint,
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(kPrimaryColor),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder([color]) {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(
+          8,
+        ),
+        borderSide: BorderSide(
+          color: color ?? Colors.white,
+        ));
+  }
+}
